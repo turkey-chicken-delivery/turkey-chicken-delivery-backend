d850656f9c67b02ce8c08e2fbd4aa0033d032d7c
92jy38 <92jy38@gmail.com>
2024-11-02 18:43:18 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/sparta/i_am_delivery/IAmDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.i_am_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAmDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IAmDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/i_am_delivery/IAmDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.i_am_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAmDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
