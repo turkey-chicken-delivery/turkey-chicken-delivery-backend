60e71cef7b4e61e40111f510a0bf523f0aed31a9
CHOSEONGJUN <lionjojo@naver.com>
2024-11-04 00:33:27 +0900

    fix : JpaAuditing 기능 어노테이션 추가 - JpaAuditing 기능이

--- src/main/java/com/sparta/i_am_delivery/IAmDeliveryApplication.java
+++ src/main/java/com/sparta/i_am_delivery/IAmDeliveryApplication.java
@@ -2,8 +2,10 @@ package com.sparta.i_am_delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IAmDeliveryApplication {
 
     public static void main(String[] args) {
