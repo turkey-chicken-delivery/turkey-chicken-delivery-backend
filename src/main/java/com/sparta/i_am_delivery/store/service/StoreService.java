e8957796b747a63484d43aa385cefcb6e0b80705
jihoon <kg5860@naver.com>
2024-11-05 01:53:29 +0900

    feat : createStore 예외처리 구현

--- src/main/java/com/sparta/i_am_delivery/store/service/StoreService.java
+++ src/main/java/com/sparta/i_am_delivery/store/service/StoreService.java
@@ -8,6 +8,8 @@ import com.sparta.i_am_delivery.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+
 @Service
 @RequiredArgsConstructor
 public class StoreService {
@@ -16,6 +18,21 @@ public class StoreService {
 
 
     public StoreResponseDto createStore(StoreRequestDto requestDto, User user){
+
+        if (!user.getType().equals("OWNER")){
+            throw new IllegalArgumentException("사장님 권한이 있는 유저만 가게를 생성할 수 있습니다.");
+        }
+
+        int storeCount = storeRepository.countByOwner(user);
+        if (storeCount >= 3) {
+            throw new IllegalArgumentException("가게는 최대 3개까지만 운영할 수 있습니다.");
+        }
+
+
+        validateStoreTimes(requestDto.getOpenTime(), requestDto.getCloseTime());
+
+        validateMinimumPrice(requestDto.getMinimumPrice());
+
 
         Store store = Store.builder()
                 .owner(user)
@@ -28,4 +45,18 @@ public class StoreService {
         Store saveStore = storeRepository.save(store);
         return new StoreResponseDto(saveStore);
     }
+
+    private void validateStoreTimes(LocalTime openTime, LocalTime closeTime) {
+
+        if (!openTime.isBefore(closeTime)) {
+            throw new IllegalArgumentException("오픈 시간은 마감 시간보다 이전이어야 합니다.");
+        }
+    }
+
+    private void validateMinimumPrice(Long minimumPrice) {
+        if (minimumPrice == null || minimumPrice <= 18000) {
+            throw new IllegalArgumentException("최소 주문 금액은 18000원 이상이어야 합니다.");
+        }
+    }
+
 }
