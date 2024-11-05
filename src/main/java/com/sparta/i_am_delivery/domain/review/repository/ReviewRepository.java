5a72125eaab54b6384ce56cd026a0eba53b8159e
lastdove <tpqms9917@gmail.com>
2024-11-06 01:20:28 +0900

    fix : 잘못 작성된 부분 수정

--- src/main/java/com/sparta/i_am_delivery/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/i_am_delivery/domain/review/repository/ReviewRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+  // JOIN FETCH를 사용해서 리뷰 ID와 사용자 ID로 리뷰를 조회하여 해당 리뷰가 특정 사용자에 의해 작성된 것인지 확인
   @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.id = :reviewId AND r.user.id = :userId")
   Optional<Review> findByIdAndUserId(@Param("reviewId") Long reviewId,
       @Param("userId") Long userId);
