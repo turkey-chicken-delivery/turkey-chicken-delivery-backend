8449231a041594cfbc8a80ce2708bcf90762e3ee
jihoon <kg5860@naver.com>
2024-11-05 23:07:22 +0900

    feat : 가게 생성 기능 추가 - ResponseDto JsonFormat 제거 및 ObjectMapperConfig 배열 -> 문자열 형식 출력 코드 추가

--- src/main/java/com/sparta/i_am_delivery/common/config/ObjectMapperConfig.java
+++ src/main/java/com/sparta/i_am_delivery/common/config/ObjectMapperConfig.java
@@ -2,6 +2,7 @@ package com.sparta.i_am_delivery.common.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,9 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-
-        ObjectMapper mapper = new ObjectMapper(); mapper.registerModule(new JavaTimeModule());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 배열 대신 문자열 형식으로 출력
         return mapper;
-
-
-
     }
 }
