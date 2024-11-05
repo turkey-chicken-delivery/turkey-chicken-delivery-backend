301f160be78a8e7d6e2415fb46c61013bdb8c799
jihoon <kg5860@naver.com>
2024-11-05 22:30:30 +0900

    feat : 가게 생성 기능 추가 - 최소주문금액 메세지 추가

--- src/main/java/com/sparta/i_am_delivery/store/dto/request/StoreCreateRequestDto.java
+++ src/main/java/com/sparta/i_am_delivery/store/dto/request/StoreCreateRequestDto.java
@@ -19,6 +19,6 @@ public class StoreCreateRequestDto {
     @NotNull(message = "마감시간을 입력해주세요.")
     @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime closeTime;
-    @Min(value = 18000)
+    @Min(value = 18000 , message = "최소 주문금액은 18000원 이상입니다.")
     private Long minimumPrice;
 }
