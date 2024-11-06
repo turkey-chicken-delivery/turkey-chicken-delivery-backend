c1286045187ac89b7b402eed3c830ccb740e0695
jihoon <kg5860@naver.com>
2024-11-06 19:53:54 +0900

    feat : 가게 폐업 기능 추가 - 가게 삭제시 , 메뉴,리뷰,주문,즐겨찾기가 삭제되는 기능 구현

--- src/main/java/com/sparta/i_am_delivery/domain/like/repository/LikeRepository.java
+++ src/main/java/com/sparta/i_am_delivery/domain/like/repository/LikeRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
   boolean existsByUserIdAndStoreId(Long userId, Long storeId);
+
+  void deleteByStoreId(Long storeId);
 }
--- src/main/java/com/sparta/i_am_delivery/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/i_am_delivery/domain/review/repository/ReviewRepository.java
@@ -1,18 +1,21 @@
 package com.sparta.i_am_delivery.domain.review.repository;
 
 import com.sparta.i_am_delivery.domain.review.entity.Review;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
   // JOIN FETCH를 사용해서 리뷰 ID와 사용자 ID로 리뷰를 조회하여 해당 리뷰가 특정 사용자에 의해 작성된 것인지 확인
   @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.id = :reviewId AND r.user.id = :userId")
   Optional<Review> findByIdAndUserId(@Param("reviewId") Long reviewId,
-      @Param("userId") Long userId);
+                                     @Param("userId") Long userId);
 
   Optional<Review> findByOrderId(Long orderId);
+
+  void deleteByStoreId(Long storeId);
 }
 
